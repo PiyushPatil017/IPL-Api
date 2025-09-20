cbf26a9c2e59fa6cf6d7e95622ed666f65fc1323
PiyushPatil017 <patil298473@gmail.com>
2025-09-20 20:57:38 +0530

    this commit will add app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+# here our api development will be done
+from flask import Flask,request
+import api
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Home'
+
+
+@app.route('/player_record')
+def player_record():
+    player = request.args.get('player')
+    response = api.player_record_api(player)
+    return response
+
+
+@app.route('/player-vs-team')
+def player_vs_team():
+    player = request.args.get('player')
+    response = api.player_vs_team_api(player)
+    return response
+
+
+@app.route('/team-vs-team')
+def team_vs_team():
+    team1 = request.args.get('team1')
+    team2 = request.args.get('team2')
+    response = api.team_vs_team_api(team1,team2)
+    return response
+
+
+@app.route('/team')
+def team():
+    team = request.args.get('team')
+    response = api.team_api(team)
+    return response
+
+
+
+
+app.run(debug=True)
